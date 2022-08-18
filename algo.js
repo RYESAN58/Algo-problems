74379d3c2110a75613b28819f87bbfe1de57ace8
Verified ✔️ <ryesan58@gmail.com>
2022-08-18 11:31:50 -0400

    Update algo.js

--- algo.js
+++ algo.js
@@ -26,4 +26,19 @@ for (let i = 1; i< arr2.length ; i++){
   arr2[i-1] = arr2[i]
   }
 arr2.pop()
+console.log(arr2)
 console.log(x2)
+
+// Insert At
+// Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+
+const insertAt = (arr, index, num) => {
+  for (let i = arr.length; i > -1; i--){
+    if (arr[i] == index){
+      arr[i] = num
+      break;
+    }else{
+    arr[i] = arr[i-1]
+    }
+  }
+}
